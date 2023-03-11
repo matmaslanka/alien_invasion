35bc2cab8eefbad0845faed4ca499319df051631
HelpMate <mates94@gmail.com>
2023-03-11 19:29:29 +0100

    settings of the game

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Klasa przechowująca wszystkie ustawienia gry."""
+
+    def __init__(self):
+        """inicjalizacja ustawień gry"""
+        self.screen_width= 1200
+        self.screen_high= 800
+        self.bg_color = (230, 230, 230)
