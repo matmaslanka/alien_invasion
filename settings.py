ccd324a603955c1482e687a011160d53c07f0ec3
HelpMate <mates94@gmail.com>
2023-03-14 21:51:45 +0100

    add settings connected with ship speed add bullet settings

--- settings.py
+++ settings.py
@@ -3,6 +3,11 @@ class Settings:
 
     def __init__(self):
         """inicjalizacja ustawień gry"""
-        self.screen_width= 1200
-        self.screen_high= 800
+        self.screen_width = 1200
+        self.screen_high = 800
         self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+        self.bullet_speed = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
