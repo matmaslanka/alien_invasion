f90fb55603fe208cb0d23a631158ce1de44c0be6
HelpMate <mates94@gmail.com>
2023-03-14 21:52:42 +0100

    implemented ship movement and bullet functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from ufo import Ufo
+
 
 class AlienInvasion:
     """Ogólna klasa przeznaczona do zarządzania zasobami i sposobem działania gry"""
@@ -13,28 +15,59 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_high))
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_high))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Inwazja obcych")
 
         self.ship = Ship(self)
+        self.ufo = Ufo(self)
 
     def run_game(self):
         """Rozpoczęcie pętli głównej gry"""
         while True:
-            #Oczekiwanie na naciśnięcie klawisza lub przycisku myszy
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            # Oczekiwanie na naciśnięcie klawisza lub przycisku myszy
+            self._check_events()
+            self.ship.update()
+            self._updete_screen()
 
-            #Odświeżanie ekranu w trakcie każdej iteracji pętli.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
 
-            #Wyświetlanie ostatnio zmodyfikowanego ekranu.
-            pygame.display.flip()
+    def _check_events(self):
+        """Reakcja na zdarzenia generowane przez klawiature i mysz."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_evens(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_evens(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _updete_screen(self):
+        """Uaktualnienie obrazów na ekranie i przejście do nowego ekranu"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        self.ufo.blitme()
+
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
-    #Utworzenie ezamplarza gry i jej uruchomienie.
+    # Utworzenie ezamplarza gry i jej uruchomienie.
 
     ai = AlienInvasion()
     ai.run_game()
