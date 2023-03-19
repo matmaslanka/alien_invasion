46cf8176a0b4f4f3f6986a6bd02f883d705e759d
HelpMate <mates94@gmail.com>
2023-03-19 21:31:50 +0100

    added new setting to project

--- settings.py
+++ settings.py
@@ -4,10 +4,16 @@ class Settings:
     def __init__(self):
         """inicjalizacja ustawień gry"""
         self.screen_width = 1200
-        self.screen_high = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_height = 800
+        self.bg_color = (0, 50, 0)  # bylo 230, 230, 230
         self.ship_speed = 1.5
-        self.bullet_speed = 3
-        self.bullet_width = 3
+        self.ship_limit = 3
+        self.bullet_speed = 3  # było 3
+        self.bullet_width = 3  # było 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (255, 255, 255)  # było 60, 60, 60
+        self.bullet_allowed = 3
+        self.ufo_speed = 2.0  # było 1.0
+        self.fleet_drop_speed = 10
+        # Wartość fleet_direction wynosząca 1 oznacza w prawo natomiast -1 oznacza lewo
+        self.fleet_direction = 1
