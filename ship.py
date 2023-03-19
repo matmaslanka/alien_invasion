f64d6b4b447d3f8d8102fe485e28e498af5b81d4
HelpMate <mates94@gmail.com>
2023-03-19 21:30:05 +0100

    add def center_ship

--- ship.py
+++ ship.py
@@ -46,3 +46,8 @@ class Ship:
     def blitme(self):
         """Wyświetlanie statku kosmicznego w jego aktualnym położeniu."""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Umieszczenie statku na środku przy dolnej krawędzi ekranu."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
