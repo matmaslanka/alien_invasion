c9d21c8278bc92203b31b18cfb60def15f42f9c5
HelpMate <mates94@gmail.com>
2023-03-21 22:06:07 +0100

    increase scale after every level

--- settings.py
+++ settings.py
@@ -6,14 +6,34 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 50, 0)  # bylo 230, 230, 230
-        self.ship_speed = 1.5
+
         self.ship_limit = 3
-        self.bullet_speed = 3  # było 3
-        self.bullet_width = 3  # było 3
+
+        self.bullet_width = 3 # było 3
         self.bullet_height = 15
         self.bullet_color = (255, 255, 255)  # było 60, 60, 60
         self.bullet_allowed = 3
-        self.ufo_speed = 2.0  # było 1.0
+
         self.fleet_drop_speed = 10
+
+        self.fleet_direction = 1
+
+        # Łatwa zmiana szybkości gry
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Inizcjalizacja ustawień, które ulegają zmianie w trakcie gry."""
+        self.ship_speed = 1.3
+        self.bullet_speed = 3  # było 3
+        self.ufo_speed = 1.5  # było 1.0
         # Wartość fleet_direction wynosząca 1 oznacza w prawo natomiast -1 oznacza lewo
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Zmiana ustawień dotyczących szybkości"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.ufo_speed *= self.speedup_scale
+
