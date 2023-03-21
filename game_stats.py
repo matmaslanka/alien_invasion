0953b5fe1732e8fe815d5f9c4815848efef71224
HelpMate <mates94@gmail.com>
2023-03-21 22:07:39 +0100

    wait for the button to be pressed

--- game_stats.py
+++ game_stats.py
@@ -7,7 +7,7 @@ class GameStats:
         self.reset_stats()
 
         # Uruchomienie gry "Alien Invasion" w stanie aktywnym.
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         """Inicjalizacja danych statystycznych, które mogą zmieniać się w trakcie gry."""
